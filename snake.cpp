e706d3399fc9386b793193cd4edce9552dbce538
iSam24 <samwhitesammy@live.co.uk>
2024-11-16 20:41:57 +0000

    user input function

--- snake.cpp
+++ snake.cpp
@@ -7,18 +7,59 @@
 using namespace std;
 
 bool gameOver;
+const int height=20;
+const int width=40;
+int x,y,fx,fy,score;
+enum Direction {STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction dir;
 
 std::vector<std::pair<int, int>> snake; // Snake body
+
 void Setup(){
     gameOver = false;
+    x = width/2;
+    y = height/2;
+    score = 0;
+    dir = STOP;       // Initial direction
+
 }
 
-void  DrawSnake(){
-    // draws the snakes body
+void  Draw(){
+    system("CLS");  // clear screen
+
+    // draw exterior of grid
+    for(int i=0; i < width; i++){
+        cout<<"_";
+    }cout<<endl;
+
+    for(int i=0; i < height; i++){
+        for(int j=0; j < width; j++){
+            if(j == 0 || j == width -1){
+                cout<<"|";
+            }
+            else {
+                cout<<" ";
+            }
+        }cout<<endl;
+    }
+    
+    for(int i=0; i < width; i++){
+        cout<<"_";
+    }cout<<endl;
+
 }
 
 void UserInput(){
     // Takes user input
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'w': dir = UP; break;
+        case 's': dir = DOWN; break;
+        case 'a': dir = LEFT; break;
+        case 'd': dir = RIGHT; break;
+        case 'x': gameOver = true; break; // Exit the game
+        }
+    }
 }
 
 void Logic(){
@@ -27,11 +68,13 @@ void Logic(){
  
  int main(){
 
+    Setup();
+
     while(!gameOver){
-        Setup();
-        DrawSnake();
+        Draw();
         UserInput();
         Logic();
         _sleep(100);
+        gameOver = true;
     }
  }
