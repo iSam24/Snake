4466d1c7c3d33ed3ed6e8e54cc507d7aeebb1239
iSam24 <samwhitesammy@live.co.uk>
2024-11-17 19:09:03 +0000

    added draw and move snake functions

--- snake.cpp
+++ snake.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <conio.h> // for _kbhit() and _getch()
-#include <cstdlib> // for rand()
-#include <ctime>   // for srand()
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
@@ -21,12 +22,18 @@ void Setup(){
     y = height/2;
     score = 0;
     dir = STOP;       // Initial direction
-
+    snake = { {width / 2, height / 2} }; // spawns a snake
 }
 
-void  Draw(){
-    system("CLS");  // clear screen
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
+void  DrawGrid(){
+    gotoxy(0, 0);
     // draw exterior of grid
     for(int i=0; i < width; i++){
         cout<<"_";
@@ -46,8 +53,50 @@ void  Draw(){
     for(int i=0; i < width; i++){
         cout<<"_";
     }cout<<endl;
+}
+
+void DrawSnake(){
+// Iterate through the snake vector, which stores the snake's body as pairs of (x, y) coordinates.
+    for(const auto& segment : snake) {
+        int x = segment.first;
+        int y = segment.second;
+
+        gotoxy(x, y);        // Move cursor to (x, y)
+        std::cout << "#";    // Draw the snake segment
+    }     
+}
+
+void MoveSnake() {
+    for (int i = snake.size() - 1; i >= 0; --i) {
+        snake[i] = snake[i - 1]; // Set current segment to the position of the previous one
+    }
+
+    switch(dir) {
+        case LEFT:
+            snake[0].first -= 1; // minus one from x
+            break;
+        case RIGHT:
+            snake[0].first += 1; // add one to x
+        case UP:
+            snake[0].second += 1; // add one to y
+        case DOWN:
+            snake[0].second -= 1; //minus one from y
+        default:
+            break; // No movement if dir == STOP
+    }
+}
+
+void DrawFruit() {
 
 }
+
+void Draw(){
+    system("CLS");  // clear screen
+    DrawGrid();
+    DrawSnake();
+    DrawFruit();
+}
+
 
 void UserInput(){
     // Takes user input
@@ -64,6 +113,12 @@ void UserInput(){
 
 void Logic(){
     // logic around the snakes body
+
+    // if snake hits wall
+
+    // if snake hits itself
+
+    // if snake eats fruit
 }
  
  int main(){
@@ -73,8 +128,8 @@ void Logic(){
     while(!gameOver){
         Draw();
         UserInput();
+        MoveSnake();
         Logic();
         _sleep(100);
-        gameOver = true;
     }
  }
